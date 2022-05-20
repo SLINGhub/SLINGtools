0d7da4f27f16650549728b5e2b84c60b450fe8be
Bo Burla <lsibjb@nus.edu.sg>
2022-05-20 19:10:31 +0800

    major updata read_masshunterCSV

--- R/coefvariation.R
+++ R/coefvariation.R
@@ -1,17 +1,17 @@
 
-#' Coefficient of Variation (CV) based on log-transformed data
+#' Coefficient of variation (CV) using log-transformed data
 #'
-#' Computes the coefficient of Variation (CV) based on log-transformed data
+#' Computes the coefficient of variation (CV) after log-transformation of the provided data
 #'
 #' @param x a numeric vector with values that are NOT log-transformed
-#' @param na.rm logical. if TRUE then NA values are stripped from x before computation takes place
+#' @param na.rm logical, if TRUE then NA values are stripped from x before computation takes place
 #'
 #' @return numeric vector of length one. If x contains a zero, then NaN is returned. If x is not numeric or integer, NA_real_ is returned
 #' @export
 #'
 #' @examples
 #'
-CVlog <- function(x, na.rm){
+cvlog <- function(x, na.rm){
   #sqrt(exp(1)^(sd(log(x, ...))^2)-1) *100
   sqrt(10^(log(10) * stats::sd(log(x, 10), na.rm )^2) - 1) * 100
 }
--- R/read_msdata.R
+++ R/read_msdata.R
@@ -53,55 +53,51 @@ read_MassHunterCSV <- function(file, silent = FALSE) {
     gsub("/", "", y))
 
 
+  datWide <- datWide |> add_row(.after = 1)
+  datWide[1, ] <- tibble(A = datWide[1,] |> unlist() |> na_if("")) |>  fill(A) |> unlist() |> as.list()
+
+  datWide[1, ] <- replace(datWide[1,], str_detect(string = datWide[1,] , pattern = "AA"),"")
+  datWide[2, ] <- replace(datWide[1,], !str_detect(string = datWide[1,] , pattern = "AA"),"")
+  # datWide[1, ] <- datWide[1, ] |> mutate(across(everything(), ~ if_else(str_detect(.x, "Qualifier \\("), "",.x)))
+  # datWide[2, ] <- datWide[2, ] |> mutate(across(everything(), ~ if_else(!str_detect(.x, "Qualifier \\("), "",.x)))
+  datWide[1, ] <- tibble(A = datWide[1,] |> unlist() |> na_if("")) |>  fill(A) |> unlist() |> as.list()
+  # Concatenate rows
+  datWide[1, ] <- paste(datWide[1, ], datWide[2, ], sep = " ") |> str_squish() |> as.list()
+  datWide <- datWide[-2, ]
+
+
   # Fill in transition name in empty columns (different parameters of the same transition)
   # Dealing with exported "Qualifier" results: Adds the corresponding quantifier name (the first column of the group) with the Tag "QUAL" and the transition in front e.g. "Sph d18:0 [QUAL 302.3>266.2]"
-  col_name = "Sample"
-  isFirstQualifier = TRUE
-  quantifier_name =
-  for (c in 1:ncol(datWide)) {
-    val = datWide[1, c]
-    #print(val)
-    if (grepl("^Qualifier \\(", val)) {
-      qualifier_transition_name <-
-        val %>% stringr::str_replace("Qualifier", "QUAL") %>% stringr::str_replace("\\(", "") %>% stringr::str_replace("\\)", "\\]") %>% stringr::str_replace(" \\-\\> ", " \\> ")
-      datWide[1, c] = paste(quantifier_name, " [", qualifier_transition_name, sep = "")
-      col_name = datWide[1, c]
-    } else if ((!is.na(val)) && nchar(val, keepNA = TRUE) > 0) {
-      col_name = val
-      quantifier_name = val
-    } else if (stringr::str_detect(val, "Method")) {
-      datWide[1, c] = datWide[1, c + 1]
-      col_name = datWide[1, c]
-    }
-    else {
-      datWide[1, c] = col_name
-    }
-  }
-  datWide[1, ] <- as.list(stringr::str_squish(unlist(datWide[1, ])))
+
+
+  # Remove columns with no column names, as they are undefined (seems to be only Outlier Summary and Quantitation Message Summary)
+  datWide <- datWide |> dplyr::select(-where(~ .x[2] == ""))
 
   # Concatenate rows containing parameters + transitions to the form parameter.sample and parameter.transition headers. This will later be converted with reshape()
   colnames(datWide) <- paste(datWide[2, ], datWide[1, ], sep = "\t")
   datWide <- datWide[-1:-2, ]
 
+
   # Rename some known column header names and remove columns that are not needed or not informative.
-  old = c(
-    "Data File\tSample",
-    "Name\tSample",
-    "AcqDate-Time\tSample",
-    "Type\tSample",
-    "Pos\tSample",
-    "Vol\tSample"
+
+  new_colnames = c(
+    "DataFileName" = "Data File\tSample",
+    "SampleName" = "Name\tSample",
+    "AcqTimeStamp" = "AcqDate-Time\tSample",
+    "SampleType" = "Type\tSample",
+    "VialPosition" = "Pos\tSample",
+    "InjVolume" = "Vol\tSample",
+    "Dilution" = "Dil\tSample",
+    "SampleGroup" = "Sample Group\tSample",
+    "InstrumentName" = "Instrument Name\tSample",
+    "InstrumentType" = "Instrument Type\tSample",
+    "AcqMethodFile" = "Acq. Method File\tSample",
+    "AcqMethodPath" = "Acq. Method Path\tSample",
+    "DataPath" = "Data Path\tSample"
   )
-  new = c(
-    "DataFileName",
-    "SampleName",
-    "AcqTimeStamp",
-    "SampleType",
-    "VialPosition",
-    "InjVolume"
-  )
-  existing <- match(old, names(datWide))
-  names(datWide)[na.omit(existing)] <- new[which(!is.na(existing))]
+
+  datWide <- datWide %>% dplyr::rename(dplyr::any_of(new_colnames))
+
 
   # Remove ".Sample" from remaining sample description headers and remove known unused columns
   datWide <-
@@ -115,6 +111,8 @@ read_MassHunterCSV <- function(file, silent = FALSE) {
 
   # Transform wide to long (tidy) table format
   # ------------------------------------------
+
+
 
   datWide <- datWide %>%
     dplyr::mutate(
@@ -148,13 +146,11 @@ read_MassHunterCSV <- function(file, silent = FALSE) {
                     .fns = as.factor)
       )
 
-  datLong <- datLong %>%
-    dplyr::rename(Intensity = .data$Area,
-                  DataName = .data$SampleName,
-                  PrecursorMZ = .data$`Precursor Ion`,
-                  ProductMZ = .data$`Product Ion`,
-                  CollisionEnergy = .data$`Collision Energy`,
-                  IonPolarity = .data$`Ion Polarity`)
+  new_colnames <- c(DataName = "SampleName", PrecursorMZ = "Precursor Ion", ProductMZ = "Product Ion",
+                    CollisionEnergy = "Collision Energy", IonPolarity = "Ion Polarity")
+
+  datLong <- datLong %>% dplyr::rename(any_of(new_colnames))
+
   if(!silent) {
     cat("Imported ", length(unique(datLong$DataFileName)), "samples with ", fill = FALSE)
     cat(length(unique(datLong$Feature)), "transitions ", fill = FALSE)
