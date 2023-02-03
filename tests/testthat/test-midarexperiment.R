505cacc31a5fe957e332e503d14f7d5156db870b
Bo Burla <bo.burla@gmail.com>
2023-02-03 18:27:34 +0800

    update tests

--- tests/testthat/test-midarexperiment.R
+++ tests/testthat/test-midarexperiment.R
@@ -8,7 +8,7 @@ testthat::test_that("import_MSOrganizerXLM: Template with all information", {
   mexp <- SLINGtools::calcQC(mexp)
 
   dd <- readRDS("21_MidarExperiment_1.rds")
-  expect_equal(dplyr::all_equal(mexp@d_QC, dd@d_QC), TRUE) &
-  expect_equal(dplyr::all_equal(mexp@dataset, dd@dataset), TRUE)
+expect_equal(all.equal(mexp@d_QC, dd@d_QC), TRUE) &
+  expect_equal(all.equal(mexp@dataset, dd@dataset), TRUE)
 })
 
